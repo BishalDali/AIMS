6aa1b69bdd633059c36647b29fc1669c6e18aa2f
Bishal Dali <bishalsthaktm12@gmail.com>
2022-04-08 21:33:16 +0545

    new API

--- api/CRUD/Read.js
+++
@@ -1,6 +0,0 @@
-const read = async (SignUpSchema) => {
-    const getAllSignupUsers = await SignUpSchema.find();
-    return getAllSignupUsers;
-};
-
-module.exports = read;
--- api/CRUD/Update.js
+++
@@ -1,9 +0,0 @@
-const update = async (SignUpSchema, id, updateData) => {
-  const updatedUser = await SignUpSchema.findByIdAndUpdate(
-    { _id: id },
-    { $set: updateData }
-  );
-  return updatedUser;
-};
-
-module.exports = update;
